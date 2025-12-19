46c8696a0c7794addab8708da7b4bd2edc1be894
Mangasriss <scan.manga.riss@gmail.com>
2025-12-19 18:12:09 +0300

    Fix: Dépendances directes dans common.gradle et injection ciblée pour Core

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,20 +14,17 @@ allprojects {
     }
 }
 
-subprojects {
+// 👇 CONFIGURATION SPÉCIALE POUR LE CORE UNIQUEMENT
+// On s'assure que le "Cerveau" (Core) a aussi les outils pour se construire
+project(":core") {
     afterEvaluate {
-        val isAndroid = plugins.hasPlugin("com.android.application") || plugins.hasPlugin("com.android.library")
-
-        if (isAndroid) {
-            dependencies {
-                add("compileOnly", "com.squareup.okhttp3:okhttp:5.0.0-alpha.11")
-                add("compileOnly", "io.reactivex:rxjava:1.3.8")
-                add("compileOnly", "io.reactivex:rxandroid:1.2.1")
-                add("compileOnly", "org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
-                add("compileOnly", "org.jsoup:jsoup:1.15.4")
-                // 👇 REMPLACEMENT : On utilise la version officielle publique au lieu de la version bloquée
-                add("compileOnly", "org.json:json:20210307") 
-            }
+        dependencies {
+            add("compileOnly", "com.squareup.okhttp3:okhttp:5.0.0-alpha.11")
+            add("compileOnly", "io.reactivex:rxjava:1.3.8")
+            add("compileOnly", "io.reactivex:rxandroid:1.2.1")
+            add("compileOnly", "org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+            add("compileOnly", "org.jsoup:jsoup:1.15.4")
+            add("compileOnly", "org.json:json:20210307")
         }
     }
 }
