2729d6a0e1e843003d9c5904bd7034287e31fe71
Mangasriss <scan.manga.riss@gmail.com>
2025-12-19 15:46:50 +0300

    Fix: Ajout des versions des plugins dans build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,20 @@
+plugins {
+    // On définit les versions EXACTES ici pour tout le projet
+    id("com.android.application") version "8.1.4" apply false
+    id("com.android.library") version "8.1.4" apply false
+    id("org.jetbrains.kotlin.android") version "1.7.20" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.7.20" apply false
+    id("org.jmailen.kotlinter") version "3.12.0" apply false
+}
+
 allprojects {
     repositories {
         mavenCentral()
         google()
         maven(url = "https://jitpack.io")
     }
+}
+
+task<Delete>("clean") {
+    delete(rootProject.buildDir)
 }
